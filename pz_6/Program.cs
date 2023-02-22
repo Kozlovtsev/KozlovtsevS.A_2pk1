0370e118c7fba7608b73585218f166cea6d10ed0
Sereja <blackatom21@mail.ru>
2023-02-22 10:01:18 +0500

    10 вариант

---
+++ pz_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинну отрезка А:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длинну отрезка В:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int box = 0;
+            int c = a - b;
+            while (c >= 0)
+            {
+                box++;
+                c = c - b;
+            }
+            Console.WriteLine($"Количество отрезков В в отрезке А: {box}");
+        }
+    }
+}
