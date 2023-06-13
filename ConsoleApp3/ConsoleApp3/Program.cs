bdf0758ee0c7c5bbd8688130ffee433639fa6d79
Kozlovtsev <114168090+Kozlovtsev@users.noreply.github.com>
2023-06-13 13:30:50 +0300

    Программа на Линуксе

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Введите длину прямоугольника:");
+
+            double length = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.WriteLine("Введите ширину прямоугольника:");
+
+            double width = Convert.ToDouble(Console.ReadLine());
+
+
+
+            double area = length * width;
+
+            double perimeter = 2 * (length + width);
+
+
+
+            Console.WriteLine($"Площадь прямоугольника: {area}");
+
+            Console.WriteLine($"Периметр прямоугольника: {perimeter}");
+
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
